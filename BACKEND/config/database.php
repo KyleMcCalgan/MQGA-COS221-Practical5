f5377fb1173e38248ee978c98b4a02db2e34ede6
marcelstoltz00 <marcelstoltz00@gmail.com>
2025-05-20 09:06:32 +0200

    'database requests fixed :)'

--- BACKEND/config/database.php
+++ BACKEND/config/database.php
@@ -4,13 +4,15 @@ function getDbConnection() {
     $name = $_ENV['DB_NAME'] ?? die('DB_NAME not set.');
     $user = $_ENV['DB_USER'] ?? die('DB_USER not set.');
     $pass = $_ENV['DB_PASS'] ?? null;
-
     try {
         $conn = new mysqli($host, $user, $pass, $name);
-        if ($conn->connect_error) die("DB Connect Failed: " . $conn->connect_error);
+        if ($conn->connect_error) {
+            die("DB Connect Failed: " . $conn->connect_error);
+        }
         $conn->set_charset("utf8mb4");
         return $conn;
     } catch (Exception $e) {
+ 
         die("DB Connection Exception: " . $e->getMessage());
     }
 }
