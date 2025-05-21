f18026e1fe3fb674aa20af0502900d6b9836ff96
J0rdanN89 <jordannaidoo89@gmail.com>
2025-05-22 00:01:43 +0200

    Fixed login page and made navbar consitent throughout website pages

--- FRONTEND/PHP/header.php
+++ FRONTEND/PHP/header.php
@@ -11,6 +11,7 @@ include "config.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" type="text/css" href="../CSS/header.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php
                                                     if ($currentPage == 'products.php') {
